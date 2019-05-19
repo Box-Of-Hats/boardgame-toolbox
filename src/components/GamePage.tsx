1b2e9987c863d3ae0d019979dfd8e35eab984b22
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-05-19 19:43:00 +0100

    make GamePage component

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import GameListing from "./GameListing";
 import Dice from "./Dice";
 import { Counter } from "./Counter";
@@ -6,67 +6,84 @@ import Spinner from "./Spinner";
 import ScoreTable from "./ScoreTable";
 interface Game {
     id: number;
-    title: string;
+    name: string;
     description: string;
+    tools: Array<any>;
 }
 
-export const GamePage = () => {
-    const getConfigData = () => {
-        return [
-            {
-                name: "Sushi GO!",
-                description: "A fast paced sushi making game.",
-                tools: [
-                    {
-                        typeId: 3,
-                        title: "ScoreTable",
-                        playerNames: ["Jake", "Alena"],
-                        scoreNames: ["Round 1", "Round 2", "Round 3", "Dessert"]
-                    }
-                ]
-            }
-        ];
-    };
+interface IGamePageProps {}
 
-    let out: any = [];
+interface IGamePageState {
+    games: Array<Game>;
+}
 
-    getConfigData().forEach(gameConfig => {
-        out.push(
-            <GameListing
-                title={gameConfig.name}
-                description={gameConfig.description}
-            />
-        );
-        gameConfig.tools.forEach(toolConfig => {
-            switch (toolConfig.typeId) {
-                case 0:
-                    out.push(
-                        <Dice
-                            diceCount={1}
-                            maximumRoll={6}
-                            title={toolConfig.title}
-                        />
-                    );
-                    break;
-                case 1:
-                    out.push(<Counter title={toolConfig.title} />);
-                    break;
-                case 2:
-                    out.push(<Spinner title={toolConfig.title} />);
-                    break;
-                case 3:
-                    out.push(
-                        <ScoreTable
-                            title={toolConfig.title}
-                            playerNames={toolConfig.playerNames}
-                            scoreNames={toolConfig.scoreNames}
-                        />
-                    );
-                    break;
-                default:
-                    break;
-            }
-        });
-    });
-    return <div>{out}</div>;
+const getConfigData = (): Array<Game> => {
+    return [
+        {
+            id: 0,
+            name: "Sushi GO!",
+            description: "A fast paced sushi making game.",
+            tools: [
+                {
+                    typeId: 3,
+                    title: "ScoreTable",
+                    playerNames: ["Jake", "Alena"],
+                    scoreNames: ["Round 1", "Round 2", "Round 3", "Dessert"]
+                }
+            ]
+        }
+    ];
 };
+
+export class GamePage extends Component<IGamePageProps, IGamePageState> {
+    constructor(props: IGamePageProps) {
+        super(props);
+        this.state = {
+            games: getConfigData()
+        };
+    }
+
+    render() {
+        let out: any = [];
+
+        this.state.games.forEach(gameConfig => {
+            out.push(
+                <GameListing
+                    title={gameConfig.name}
+                    description={gameConfig.description}
+                />
+            );
+            gameConfig.tools.forEach(toolConfig => {
+                switch (toolConfig.typeId) {
+                    case 0:
+                        out.push(
+                            <Dice
+                                diceCount={1}
+                                maximumRoll={6}
+                                title={toolConfig.title}
+                            />
+                        );
+                        break;
+                    case 1:
+                        out.push(<Counter title={toolConfig.title} />);
+                        break;
+                    case 2:
+                        out.push(<Spinner title={toolConfig.title} />);
+                        break;
+                    case 3:
+                        out.push(
+                            <ScoreTable
+                                title={toolConfig.title}
+                                playerNames={toolConfig.playerNames}
+                                scoreNames={toolConfig.scoreNames}
+                            />
+                        );
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+        return <div>{out}</div>;
+    }
+}
