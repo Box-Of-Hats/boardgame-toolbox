c1929b1e508a6c05349b489eddaa826b56f6b830
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-05-19 23:44:13 +0100

    add indexDB support

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -4,27 +4,57 @@ import Dice from "./Dice";
 import { Counter } from "./Counter";
 import Spinner from "./Spinner";
 import ScoreTable from "./ScoreTable";
-import { getGames } from "../dataManager";
+import { GamesDatabase } from "../GamesDatabase";
 import { Game } from "../types/Game.interface";
 
 interface IGamePageProps {}
 
 interface IGamePageState {
-    games: Array<Game>;
+    gamesDb: GamesDatabase;
+    games: Game[];
 }
 
 export class GamePage extends Component<IGamePageProps, IGamePageState> {
     constructor(props: IGamePageProps) {
         super(props);
         this.state = {
-            games: getGames()
+            gamesDb: new GamesDatabase(),
+            games: []
         };
     }
 
     render() {
-        let out: any = [];
+        var out: any[] = [];
+
+        this.state.gamesDb
+            .loadGames()
+            .then(
+                g => {
+                    if (g.length === 0) {
+                        // No games in local indexDB
+                        let seedGames = this.state.gamesDb.addSeedGames();
+                        this.setState({
+                            games: seedGames
+                        });
+                    } else if (this.state.games.length !== g.length) {
+                        // Number of games in local indexDB is more than currently loaded
+                        this.setState({
+                            games: g
+                        });
+                    } else {
+                        // Dont set state and cause re-render
+                    }
+                },
+                f => {
+                    //Reject
+                }
+            )
+            .catch(f => {
+                //Error
+            });
 
         this.state.games.forEach(gameConfig => {
+            console.log(`Found game: ${gameConfig.name}`);
             out.push(
                 <GameListing
                     title={gameConfig.name}
@@ -38,7 +68,7 @@ export class GamePage extends Component<IGamePageProps, IGamePageState> {
                             <Dice
                                 diceCount={1}
                                 maximumRoll={6}
-                                title={toolConfig.title}
+                                title={toolConfig.name}
                             />
                         );
                         break;
@@ -62,6 +92,7 @@ export class GamePage extends Component<IGamePageProps, IGamePageState> {
                 }
             });
         });
+
         return <div>{out}</div>;
     }
 }
