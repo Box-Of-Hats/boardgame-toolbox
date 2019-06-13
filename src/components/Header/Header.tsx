d62562c2abc35578f5c5b21eed2aa20d9302276a
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-06-13 23:33:48 +0100

    add action to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,10 @@ import {Link} from 'react-router-dom';
 interface IHeaderProps {
     title?: string;
     backLink?: string;
+    actionCallback?: Function;
+    actionIcon?: string;
+    actionCallbackRedirect?: string;
+    actionIconStyle?: object;
 }
 
 class Header extends React.Component<IHeaderProps, any> {
@@ -23,7 +27,22 @@ class Header extends React.Component<IHeaderProps, any> {
                 <div className='header__title'>
                     {this.props.title ? this.props.title : 'Header'}
                 </div>
-                <div className='header__link header__link--other'></div>
+                {this.props.actionCallback && (
+                    <Link to={this.props.actionCallbackRedirect}>
+                        <div
+                            className='header__link header__link--other'
+                            onClick={() => this.props.actionCallback()}>
+                            <i
+                                style={this.props.actionIconStyle}
+                                className='material-icons'>
+                                {this.props.actionIcon
+                                    ? this.props.actionIcon
+                                    : 'touch_app'}
+                            </i>
+                        </div>
+                    </Link>
+                )}
+                {!this.props.actionCallback && <div></div>}
             </div>
         );
     }
