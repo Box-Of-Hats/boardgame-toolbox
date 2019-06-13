c2223ad961671aed00b6b0d94762774959ba1f8f
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-06-13 21:53:42 +0100

    add beforeinstallprompt

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,14 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', e => {
+    console.log('before install prompt');
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    deferredPrompt.prompt();
+});
